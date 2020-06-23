22bcaa0d4d588dca9da60b81b3d3741b5c0deca9
Kyle Minors <kyleminors@gmail.com>
2020-06-23 21:10:37 +1000

    Initial commit

---
+++ Assets/Scripts/RunScene.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RunScene : MonoBehaviour
+{
+    public VisualEffect[] visualEffects;
+
+    public Rigidbody[] cubes;
+
+
+    public string sceneName;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float targetHeight;
+
+    private Button startButton;
+
+    void Start()
+    {
+        startButton = GameObject.FindGameObjectWithTag("startButton").GetComponent<Button>();
+        startButton.onClick.AddListener(LoadScene);
+        RaiseBlocks(); 
+    }
+
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName); 
+    }
+
+    private void RaiseBlocks()
+    {
+
+        foreach(Rigidbody cube in cubes)
+        {
+            cube.AddForce(Vector3.up * (Time.deltaTime + speed));
+            cube.gameObject.transform.Rotate(0, 1, 0); 
+        }
+
+
+      
+    }
+
+    
+}
